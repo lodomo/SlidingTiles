6bc90937ff2f0733ba7c37e0ed742f03328740fd
Lorenzo Moon <lhaskins@cecs.pdx.edu>
2024-07-11 15:18:05 -0700

    Fixing heuristics and gitignore

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 command='python slidingtiles.py'
 solv_dir='./solvable/'
 algos=(1 2)
-heuristics=(1 2)
+heuristics=(1 2 3)
 rounds=5
 
 pipenv shell
@@ -20,13 +20,14 @@ $command -g $rounds
 # Put the files in solvable into a list of files
 files=$(ls solvable)
 
+echo $files
+
 # Run the tests
 for algo in ${algos[@]}; do
     for heuristic in ${heuristics[@]}; do
         for file in $files; do
-            for i in $(seq 1 $rounds); do
-                $command -a $algo -h $heuristic < $solv_dir$file
-            done
+            echo "$command -a $algo -h $heuristic < $solv_dir$file"
+            $command -a $algo -H $heuristic < $solv_dir$file
         done
     done
 done
