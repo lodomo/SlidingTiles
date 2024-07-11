ca54f5b050c92d752a59a61d169f412e0376511e
Lorenzo Moon <lhaskins@cecs.pdx.edu>
2024-07-10 23:31:00 -0700

    Progress but still lots of work tod o

--- slidingtiles.py
+++ slidingtiles.py
@@ -12,8 +12,11 @@
 #
 ###############################################################################
 
+import datetime
 import getopt
+import os
 import sys
+import re
 
 import numpy as np
 
@@ -22,13 +25,19 @@ MATRIX_DIM = 3
 
 
 def main(argv):
+    sys.setrecursionlimit(10000)
     process_command_line(argv)
     puzzle = set_puzzle()
 
-    # misplaced = h1_misplaced(puzzle)
-    # manhattan = h2_manhattan(puzzle)
+    verbose(f"Starting Search\n", 2)
+    verbose(f"Start State: {b_replace(puzzle)}\n")
 
-    legal_moves(puzzle)
+    previous_states = {}
+    solution = SETTINGS["Algorithm"](puzzle, previous_states)
+    if solution is None:
+        print("No solution found")
+    else:
+        generate_report(puzzle, solution)
     exit(0)
 
 
@@ -42,17 +51,20 @@ def set_puzzle():
         print("The puzzle is not solvable. Exiting...")
         exit(1)
 
-    return user_puzzle()
+    return puzzle
 
 
 def process_command_line(argv):
-    options = "hvsrH"
+    options = "hvsrH:a:"
     DEFAULT_SIZE = 9
     HEURISTICS = [h1_misplaced, h2_manhattan, h3_tbd]
+    ALGORITHMS = [best_first_search, a_star]
     SETTINGS["verbose"] = 0  # Verbose mode
     SETTINGS["random"] = False  # Random mode
     SETTINGS["size"] = DEFAULT_SIZE  # Size of the puzzle
     SETTINGS["Heuristic"] = None  # Heuristic mode
+    SETTINGS["Algorithm"] = None  # Algorithm mode
+    SETTINGS["solve_state"] = solved_state()
 
     try:
         opts, args = getopt.getopt(argv, options)
@@ -69,18 +81,28 @@ def process_command_line(argv):
             verbose(f"Verbose mode: {SETTINGS['verbose']}\n")
         elif opt in ("-r", "--random"):
             SETTINGS["random"] = True
+        elif opt in ("-s", "--size"):
+            SETTINGS["size"] = int(arg)
+            SETTINGS["solve_state"] = solved_state()
         elif opt in ("-H", "--heuristic"):
             heuristic = int(arg)
-            heuristic -= 1
-            if heuristic < 0 or heuristic >= len(HEURISTICS):
+            if heuristic < 1 or heuristic > len(HEURISTICS):
                 print(f"Invalid heuristic: {arg}")
+                help_simple()
                 print("Run with -h for help")
                 sys.exit(2)
-            SETTINGS["Heuristic"] = HEURISTICS[int(arg)]
-            help_simple()
-            sys.exit()
+            SETTINGS["Heuristic"] = HEURISTICS[heuristic - 1]
+        elif opt in ("-a", "--algorithm"):
+            algorithm = int(arg)
+            if algorithm < 1 or algorithm > len(ALGORITHMS):
+                print(f"Invalid algorithm: {arg}")
+                help_simple()
+                print("Run with -h for help")
+                sys.exit(2)
+            SETTINGS["Algorithm"] = ALGORITHMS[algorithm - 1]
 
     SETTINGS["matrix_dim"] = int(SETTINGS["size"] ** 0.5)
+    verbose(f"Size: {SETTINGS['size']}\n")
 
     if SETTINGS["matrix_dim"] ** 2 != SETTINGS["size"]:
         print("Invalid size: Size is not a square number")
@@ -89,6 +111,16 @@ def process_command_line(argv):
     if SETTINGS["Heuristic"] is None:
         SETTINGS["Heuristic"] = h1_misplaced
 
+    if SETTINGS["Algorithm"] is None:
+        SETTINGS["Algorithm"] = best_first_search
+
+    verbose("Settings:\n")
+    verbose(f"Verbose: {SETTINGS['verbose']}\n")
+    verbose(f"Random: {SETTINGS['random']}\n")
+    verbose(f"Size: {SETTINGS['size']}\n")
+    verbose(f"Heuristic: {SETTINGS['Heuristic'].__name__}\n")
+    verbose(f"Algorithm: {SETTINGS['Algorithm'].__name__}\n")
+    verbose(f"Solve State: {b_replace(SETTINGS['solve_state'])}\n")
     return
 
 
@@ -152,7 +184,14 @@ def user_puzzle():
         print("Invalid puzzle: Not a permutation of b, 1 to n")
         exit(1)
 
+    # Update global settings
+    SETTINGS["size"] = len(puzzle)
+    SETTINGS["matrix_dim"] = int(len(puzzle) ** 0.5)
+    SETTINGS["solve_state"] = solved_state()
     verbose(f"\nUser puzzle: {b_replace(puzzle)}\n")
+    verbose(f"Size: {SETTINGS['size']}\n")
+    verbose(f"Matrix Dim: {SETTINGS['matrix_dim']}\n")
+    verbose(f"Solve State: {b_replace(SETTINGS['solve_state'])}\n")
     return puzzle
 
 
@@ -290,6 +329,113 @@ def legal_moves(puzzle):
         verbose(f"{b_replace(move, True)}\n", 2)
     verbose("End Legal Moves\n", 2)
     return moves
+
+
+def a_star(puzzle):
+    print("A* Not implemented")
+    pass
+
+
+def best_first_search(puzzle, previous_states):
+    # Base Case Return the Solved State
+    h_val = SETTINGS["Heuristic"](puzzle)
+    if h_val == 0:
+        # An empty list is returned to signal the end of the solution
+        return []
+
+    # Otherwise we need to store the solution on the way back
+    solution = []
+    previous_states[bad_hash(puzzle)] = True
+    moves = legal_moves(puzzle)
+    m_h = []
+    for move in moves:
+        m_h.append([SETTINGS["Heuristic"](move), move])
+
+    # sort by heuristic
+    m_h.sort(key=lambda x: x[0])
+
+    verbose("Start Best-First Search\n", 2)
+    verbose(f"Start State: \n{b_replace(puzzle)}\n", 2)
+    verbose(f"Start Heuristic: {h_val}\n", 2)
+    verbose(f"Moves: {len(moves)}\n", 2)
+    verbose("Moves: \n", 2)
+    for move in m_h:
+        verbose(f"{b_replace(move[1])} Heuristic: {move[0]}\n", 2)
+
+    for move in m_h:
+        if bad_hash(move[1]) in previous_states:
+            continue
+
+        verbose(f"Move: {b_replace(move[1])}\n", 2)
+
+        temp = best_first_search(move[1], previous_states)
+        if temp is not None:
+            solution = [move[1]] + temp
+            return solution
+
+    # No solution found
+    return None
+
+
+def solved_state():
+    solved = np.array(range(1, SETTINGS["size"] + 1))
+    solved[SETTINGS["size"] - 1] = 0
+    return solved
+
+
+def bad_hash(puzzle):
+    str = ""
+    for i in range(len(puzzle)):
+        str += "{:02d}".format(puzzle[i])
+    return str
+
+
+def generate_report(puzzle, solution):
+    name_of_algorithm = SETTINGS["Algorithm"].__name__
+    name_of_heuristic = SETTINGS["Heuristic"].__name__
+
+    if name_of_algorithm == "best_first_search":
+        name_of_algorithm = "Best-First Search"
+    else:
+        name_of_algorithm = "A* Algorithm"
+
+    if name_of_heuristic == "h1_misplaced":
+        name_of_heuristic = "Misplaced Tiles"
+    elif name_of_heuristic == "h2_manhattan":
+        name_of_heuristic = "Manhattan Distance"
+    else:
+        name_of_heuristic = "TBD"
+
+    # Create the directory if it doesn't exist
+    directory = "./reports/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Create the file if it doesn't exist
+    noa = name_of_algorithm.replace(" ", "_")
+    noh = name_of_heuristic.replace(" ", "_")
+    file_name = f"{directory}{noa}_{noh}.txt"
+
+    if not os.path.exists(file_name):
+        header = f"Algorithm: {name_of_algorithm}\n"
+        header += f"Heuristic: {name_of_heuristic}\n"
+        with open(file_name, "w") as file:
+            file.write(header)
+
+    string = f"{len(solution)}: \t"
+    puzzle_as_string = b_replace(puzzle)
+    puzzle_as_string = puzzle_as_string.replace("\n", "_")
+    puzzle_as_string = puzzle_as_string.replace(" ", "")
+    puzzle_as_string = puzzle_as_string.replace("[", "")
+    puzzle_as_string = puzzle_as_string.replace("]", "")
+    string += f"{puzzle_as_string} -> "
+    for state in solution:
+        string += f"{b_replace(state)}->"
+    string = string[:-2] + "\n"
+
+    # Append to the file 
+    with open(file_name, "a") as file:
+        file.write(string)
 
 
 if __name__ == "__main__":
